ce116adbbdd39d7365d3c74d6139de6e90824031
isaccochiaf <isacco.chiaf@gmail.com>
2015-10-28 22:37:33 +0100

    fasda

--- js/video_control.js
+++ js/video_control.js
@@ -5,14 +5,14 @@
  var textreplay = '<i class="fa fa-undo"></i>'
 
 
- var videotitle = document.getElementById("title");
+ //var videotitle = document.getElementById("title");
 
 
 
  video.addEventListener('ended', function () {
      // only functional if "loop" is removed 
      video.pause();
-     videotitle.classList.toggle("opac-totale");
+     //videotitle.classList.toggle("opac-totale");
      video.classList.toggle("opac-off");
      pauseButton.innerHTML = textreplay;
      pauseButton.classList.toggle("opac");
@@ -21,7 +21,7 @@
  pauseButton.addEventListener("click", function () {
      video.classList.toggle("opac-off");
      pauseButton.classList.toggle("opac");
-     videotitle.classList.toggle("opac-totale");
+     //videotitle.classList.toggle("opac-totale");
 
      if (video.paused) {
          video.play();
